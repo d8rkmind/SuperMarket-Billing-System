d95ec301de04409383b7225e9831d3cdad0f0828
Syam SV <syamsv2020@gmail.com>
2022-05-25 16:03:48 +0530

    First commit

---
+++ store/models.py
@@ -0,0 +1,9 @@
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+class Store(models.Model):
+    name=models.CharField(max_length=255)
+    category=models.CharField(max_length=255)
+    subcategory=models.CharField(max_length=255)
+    amount = models.IntegerField()
